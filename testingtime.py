dda28b3ef594b1511d706d32d1bc9b97a04d36eb
Alex Portillo <s8112353@live.vu.edu.au>
2024-08-02 13:37:32 +1000

    Personalising the code to the given prompt

--- testingtime.py
+++ testingtime.py
@@ -4,10 +4,13 @@ ORANGE = "\033[93m"
 RED = "\033[91m"
 CLEAR = "\033c"
 
+website1 = ''
+email1 = ''
+passw = ''
 
 # Give the user some context.
 print(CLEAR)
-print(GREEN + "\nThis program…………………………" + RESET)
+print(GREEN + "\nDIGICORE PASSWORD MANAGER" + RESET)
 
 # Set an initial value for choice other than the value for 'quit'.
 choice = ''
@@ -15,23 +18,20 @@ choice = ''
 # Start a loop that runs until the user enters the value for 'quit'.
 while choice != 'q':
     # Give all the choices in a series of print statements.
-    print("\n[1] Enter 1 to create an encryption key.")
-    print("[2] Enter 2 to …….")
-    print("[3] Enter 3 to……")
-    print("[q] Enter q to quit.")
+    print("\n[A] Add Credentials")
+    print("[V] View Credentials")
+    print("[Q] Quit Password Manager")
     
     # Ask for the user's choice.
     choice = input("\nMake your choice: ").lower().strip()
     
     # Respond to the user's choice.
-    if choice == '1':
-        print("\nEnter a name for the encryption key\n")
-    elif choice == '2':
+    if choice == 'a':
+        print(CLEAR + "\nWhat website credentials will you be storing?")
+    elif choice == 'v':
         print("\nEnter …….\n")
-    elif choice == '3':
-        print("\nEnter ……\n")
     elif choice == 'q':
-        print("\nExiting the menu\n")
+        print("\nExiting Password Manager\n")
     else:
         print(ORANGE + "\nInvalid option, please try again.\n" + RESET)
         
